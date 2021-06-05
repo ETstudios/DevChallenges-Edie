4e44965bdeadf7968203dba1d04cc14d6dcf3abd
Eric Thomas <edt514@gmail.com>
2021-06-04 23:22:23 -0400

    Add project assets

---
+++ index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="author" content="Eric Thomas">
+	<meta name="description" content="Eric Thomas recreation of the DevChallenges Edie challenge">
+	<title> Edie </title>
+	<link rel="shortcut icon" href="img/icon.png">    
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/edie.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+	<script src="js/bootstrap.min.js"></script>
+</head>
+<body>
+    
+</body>
+</html>
